c4385f294fac125b95ad6f6e15c0e3c4972f6dc9
surray <surejuly@gmail.com>
2023-06-02 07:11:06 +0530

    0711

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,7 +1,7 @@
 import React,{useEffect,useState} from 'react';
 import axios from "axios";
 import {toast} from "react-toastify";
-import AddTaskIcon from '@mui/icons-material/AddTask';
+import DoneOutlineIcon from '@mui/icons-material/DoneOutline';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import { Link } from 'react-router-dom';
@@ -69,7 +69,7 @@ const Home = () => {
 
                 <form onSubmit={handleSubmit}>
                     <input type='text' autoComplete="off" placeholder='Type the task title here' name='tasktitle' style={{ width: "250px" }} onChange={handleInputChange} />
-                    <input type="date" onChange={handleInputChange} name='date' style={{ width: "150px" }} />
+                    <input type="date" onChange={handleInputChange} name='date'   style={{ width: "150px" }} />
                     <input type='time' onChange={handleInputChange} name='time' style={{ width: "90px" }} />
                     <input type='submit' style={{color:'rgb(35,65,89)',background:'rgb(194,231,255)',borderRadius:'5px'}} value="Add Task" />
                 </form>
@@ -81,7 +81,7 @@ const Home = () => {
                         <a href="#" className="list-group-item list-group-item-action" aria-current="true">
                             <div className="d-flex w-100 justify-content-between">
 
-                                <AddTaskIcon  style={{color:"rgb(54,129,224)"}}onClick={() => deleteItem(item.id)} /> <h5 className="mb-1" id="task">{item.tasktitle}</h5>
+                                <DoneOutlineIcon  style={{color:"rgb(54,129,224)"}}onClick={() => deleteItem(item.id)} /> <h5 className="mb-1" id="task">{item.tasktitle}</h5>
                                 <small>{new Date(item.date).toLocaleDateString()}</small>
                                 <small>{item.time}</small>
                                 <Link to={`/update/${item.id}`}>
